c31cb48e62d95a253a5610908333e6da42190206
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-06-04 15:20:50 +0700

    /*jika selain int maka block ini akan dieksekusi*/

--- manualexception.cpp
+++ manualexception.cpp
@@ -6,5 +6,17 @@ int main()
     try{
         cout << "Selamat belajar di prodi TI UMY" << endl;
         throw 0.5; //melemparkan sebuah int maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+
     }
+
+    catch(int a) {
+        // blok akan dieksekusi
+        cout << "pengecualian akan dieksekusi" << endl;
+    }
+    catch(...) {
+        /*jika selain int maka block ini akan dieksekusi*/
+        cout << "default pengecualian dieksekusi" << endl;
+    }
+    return 0;
 }
