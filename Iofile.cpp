2146cb180a7aa431daed385c82a47e3cea217e81
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-06-04 14:31:52 +0700

    membuat string baris

--- Iofile.cpp
+++ Iofile.cpp
@@ -2,3 +2,13 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main() {
+    string baris;
+    //membuka file dalam mode menulis ofstream 
+    outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt")
+
+    
+}
