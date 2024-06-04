33564f78fb11a64256d894c0366d3d07c1b7ba41
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-06-04 14:33:25 +0700

    membuat cout

--- Iofile.cpp
+++ Iofile.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main() {
     string baris;
     //membuka file dalam mode menulis ofstream 
-    outfile;
+     ofstream outfile;
     //menunjuk ke sebuah nama file
-    outfile.open("contohfile.txt")
+    outfile.open("contohfile.txt");
 
-    
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 }
