c8431758f59f75f0273674015d2b1b523a2de921
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-06-04 15:17:52 +0700

    /melemparkan sebuah int maka

--- manualexception.cpp
+++ manualexception.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main
+int main()
+{
+    try{
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        throw 0.5; //melemparkan sebuah int maka
+    }
+}
