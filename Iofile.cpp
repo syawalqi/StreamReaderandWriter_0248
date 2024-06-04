c427ce7f0ec22002bdeae09ef40eb3195ce31ae1
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-06-04 14:49:14 +0700

    //melakukan perulangan setiap baris

--- Iofile.cpp
+++ Iofile.cpp
@@ -33,6 +33,11 @@ int main() {
     cout << endl << ">= Membuka dan membaca file" << endl;
     // jika file ada maka
     if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while(getline(infile, baris))
+        {}
+    }
 
 
 }
